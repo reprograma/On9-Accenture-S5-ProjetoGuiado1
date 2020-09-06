9fd761ca21138b9e0b2f0cfb76c080e41a1ac0d7
EllenCoe <ellen.xavier1@hotmail.com>
2020-09-06 12:04:36 -0300

    Adicionando filtros

--- pokedex/js/app.js
+++ pokedex/js/app.js
@@ -1,36 +1,115 @@
 let pokemonList = [];
 
-function filterPokemon(name, type) {
-    // Seu código aqui
+function filterPokemon(name, type, pokemonList) {
+  let list = pokemonList;
+  if (name !== "") {
+    list = pokemonList.filter((item) => {
+      console.log(item);
+      return item.name == name;
+    });
+  }
 
-    // Retorne a lista filtrada
-    return [];
+  if (type) {
+    list = pokemonList.filter((item) => {
+      const types = item.type;
+      const hasType = types.find((tipo) => tipo == type);
+      if (hasType) {
+        return item;
+      }
+    });
+  }
+  return list;
 }
 
 function sortPokemon(filteredList, sortExpression) {
-    // Seu código aqui
+  let list = filteredList;
+  switch (sortExpression) {
+    case "ID (asc)":
+      return list.sort(function (currentItem, anteriorItem) {
+        if (currentItem.id < anteriorItem.id) {
+          return -1;
+        }
+        if (currentItem.id > anteriorItem.id) {
+          return 1;
+        }
+        return 0;
+      });
 
-    // Retorne a lista ordenada
-    return [];
+    case "ID (desc)":
+      return list
+        .sort(function (currentItem, anteriorItem) {
+          if (currentItem.id < anteriorItem.id) {
+            return -1;
+          }
+          if (currentItem.id > anteriorItem.id) {
+            return 1;
+          }
+          return 0;
+        })
+        .reverse();
+    case "A-Z":
+      return list.sort(function (currentItem, anteriorItem) {
+        if (currentItem.name < anteriorItem.name) {
+          return -1;
+        }
+        if (currentItem.name > anteriorItem.name) {
+          return 1;
+        }
+        return 0;
+      });
+    case "Z-A":
+      return list
+        .sort(function (currentItem, anteriorItem) {
+          if (currentItem.name < anteriorItem.name) {
+            return -1;
+          }
+          if (currentItem.name > anteriorItem.name) {
+            return 1;
+          }
+          return 0;
+        })
+        .reverse();
+  }
+
+  return list;
 }
 
 function deletePokemon(id) {
-    // Seu código aqui
+  // Seu código aqui
 }
 
-function addPokemon(name, hp, attack, defense, speed, specialAttack, specialDefense, types) {
-    // Seu código aqui
-    // Atenção: types vem como uma stringona, cabe a você transformar num array
+function addPokemon(
+  name,
+  hp,
+  attack,
+  defense,
+  speed,
+  specialAttack,
+  specialDefense,
+  types
+) {
+  // Seu código aqui
+  // Atenção: types vem como uma stringona, cabe a você transformar num array
 }
 
 function getPokemon(id) {
-    // Seu código aqui
+  // Seu código aqui
 
-    // Retorne um pokemon da lista que tenha o id enviado
-    return {};
+  // Retorne um pokemon da lista que tenha o id enviado
+  return {};
 }
 
-function editPokemon(id, name, hp, attack, defense, speed, specialAttack, specialDefense, types) {
-    // Seu código aqui
-    // Atenção: types vem como uma stringona, cabe a você transformar num array
+function editPokemon(
+  id,
+  name,
+  hp,
+  attack,
+  defense,
+  speed,
+  specialAttack,
+  specialDefense,
+  types
+) {
+  // Seu código aqui
+  // Atenção: types vem como uma stringona, cabe a você transformar num array
 }
--- pokedex/js/main.js
+++ pokedex/js/main.js
@@ -1,76 +1,108 @@
 $(document).ready(function () {
-  let pokemonTypes = ["bug", "dragon", "electric", "fairy", "fighting", "fire", "flying", "ghost", "grass", "ground", "ice", "normal", "poison", "psychic", "rock", "steel", "water", "dark"]
+  let pokemonTypes = [
+    "bug",
+    "dragon",
+    "electric",
+    "fairy",
+    "fighting",
+    "fire",
+    "flying",
+    "ghost",
+    "grass",
+    "ground",
+    "ice",
+    "normal",
+    "poison",
+    "psychic",
+    "rock",
+    "steel",
+    "water",
+    "dark",
+  ];
 
-  fetch('https://borgesdn.github.io/pokedex-source/pokedex.json')
-    .then(res => res.json())
-    .then(json => {
-      pokemonList = json
-      load(pokemonList)
-    })
+  fetch("https://borgesdn.github.io/pokedex-source/pokedex.json")
+    .then((res) => res.json())
+    .then((json) => {
+      pokemonList = json;
+      load(pokemonList);
+    });
 
-  $('#filter-name').on('keyup', e => {
-    filter()
-  })
+  $("#filter-name").on("keyup", (e) => {
+    filter();
+  });
 
-  $('#filter-type').on('change', e => {
-    e.preventDefault()
-    filter()
-    $('#filter-type').blur()
-  })
+  $("#filter-type").on("change", (e) => {
+    e.preventDefault();
+    filter();
+    $("#filter-type").blur();
+  });
 
-  $('#sort-type').on('change', e => {
-    e.preventDefault()
-    sort()
-    $('#sort-type').blur()
-  })
+  $("#sort-type").on("change", (e) => {
+    e.preventDefault();
+    sort();
+    $("#sort-type").blur();
+  });
 
-  $('.btn-new').on('click', function (e) {
-    e.preventDefault()
-    cleanForm()
-    $(".modal-title").html("Cadastrar Pokemon")
-    $(".modal").modal('show')
-  })
+  $(".btn-new").on("click", function (e) {
+    e.preventDefault();
+    cleanForm();
+    $(".modal-title").html("Cadastrar Pokemon");
+    $(".modal").modal("show");
+  });
 
   function cleanForm() {
-    $("#id").val(0)
-    $("#name").val("")
-    $("#hp").val("")
-    $("#atk").val("")
-    $("#def").val("")
-    $("#speed").val("")
-    $("#satk").val("")
-    $("#def").val("")
+    $("#id").val(0);
+    $("#name").val("");
+    $("#hp").val("");
+    $("#atk").val("");
+    $("#def").val("");
+    $("#speed").val("");
+    $("#satk").val("");
+    $("#def").val("");
 
-    $('.type').prop('checked', false)
+    $(".type").prop("checked", false);
   }
 
-  $(".btn-save").on('click', function (e) {
-    e.preventDefault()
+  $(".btn-save").on("click", function (e) {
+    e.preventDefault();
 
-    let types = ''
-    $('.type:checked').each(function () {
+    let types = "";
+    $(".type:checked").each(function () {
       types += `${$(this).val()};`;
     });
 
-    if ($("#id").val() == '0') {
-      addPokemon($("#name").val(), $("#hp").val(),
-        $("#atk").val(), $("#def").val(),
-        $("#speed").val(), $("#satk").val(),
-        $("#def").val(), types)
+    if ($("#id").val() == "0") {
+      addPokemon(
+        $("#name").val(),
+        $("#hp").val(),
+        $("#atk").val(),
+        $("#def").val(),
+        $("#speed").val(),
+        $("#satk").val(),
+        $("#def").val(),
+        types
+      );
     } else {
-      editPokemon($("#id").val(), $("#name").val(), $("#hp").val(),
-        $("#atk").val(), $("#def").val(),
-        $("#speed").val(), $("#satk").val(),
-        $("#def").val(), types)
+      editPokemon(
+        $("#id").val(),
+        $("#name").val(),
+        $("#hp").val(),
+        $("#atk").val(),
+        $("#def").val(),
+        $("#speed").val(),
+        $("#satk").val(),
+        $("#def").val(),
+        types
+      );
     }
-    load(pokemonList)
-  })
+    load(pokemonList);
+  });
 
   function filter() {
-    const name = $('#filter-name').val()
-    const type = $('#filter-type').val()
+    const name = $("#filter-name").val();
+    const type = $("#filter-type").val();
 
-    const filteredList = filterPokemon(name, type);
+    const filteredList = filterPokemon(name, type, pokemonList);
     load(filteredList);
 
     return filteredList;
@@ -78,7 +110,7 @@ $(document).ready(function () {
 
   function sort() {
     const filteredList = filter();
-    const sortExpression = $('#sort-type').val();
+    const sortExpression = $("#sort-type").val();
     const sortedList = sortPokemon(filteredList, sortExpression);
 
     load(sortedList);
@@ -87,63 +119,69 @@ $(document).ready(function () {
   function edit(e) {
     e.preventDefault();
 
-    const pokemon = getPokemon($(this).data("id"))
+    const pokemon = getPokemon($(this).data("id"));
 
-    $("#id").val(pokemon.id)
-    $("#name").val(pokemon.name)
-    $("#hp").val(pokemon.stats.hp)
-    $("#atk").val(pokemon.stats.attack)
-    $("#def").val(pokemon.stats.defense)
-    $("#speed").val(pokemon.stats.speed)
-    $("#satk").val(pokemon.stats['sp-atk'])
-    $("#sdef").val(pokemon.stats['sp-def'])
+    $("#id").val(pokemon.id);
+    $("#name").val(pokemon.name);
+    $("#hp").val(pokemon.stats.hp);
+    $("#atk").val(pokemon.stats.attack);
+    $("#def").val(pokemon.stats.defense);
+    $("#speed").val(pokemon.stats.speed);
+    $("#satk").val(pokemon.stats["sp-atk"]);
+    $("#sdef").val(pokemon.stats["sp-def"]);
 
     for (const type of pokemon.type) {
-      $(`.type[value=${type}`).prop('checked', true)
+      $(`.type[value=${type}`).prop("checked", true);
     }
 
-    $(".modal-title").html(`Editar ${pokemon.name}`)
-    $(".modal").modal('show')
+    $(".modal-title").html(`Editar ${pokemon.name}`);
+    $(".modal").modal("show");
   }
 
   function exclude(e) {
     e.preventDefault();
 
-    $('#filter-name').val("")
-    $('#filter-type').val("")
-    $('#sort-type').val("")
-    deletePokemon($(this).data("id"))
+    $("#filter-name").val("");
+    $("#filter-type").val("");
+    $("#sort-type").val("");
+    deletePokemon($(this).data("id"));
 
-    load(pokemonList)
+    load(pokemonList);
   }
 
   function load(pokedex) {
-    const view = pokedex
-      .map(p => createCard(p))
-      .join('')
-    $('.pokedex').html(view + '<div class="warning hidden">Nenhum pokemon foi encontrado.</div>')
+    const view = pokedex.map((p) => createCard(p)).join("");
+    $(".pokedex").html(
+      view + '<div class="warning hidden">Nenhum pokemon foi encontrado.</div>'
+    );
 
-    $(".btn-edit").off("click")
-    $(".btn-edit").on("click", edit)
+    $(".btn-edit").off("click");
+    $(".btn-edit").on("click", edit);
 
-    $(".btn-trash").off("click")
-    $(".btn-trash").on("click", exclude)
+    $(".btn-trash").off("click");
+    $(".btn-trash").on("click", exclude);
   }
 
   function createCard(pokemon) {
     const types = pokemon.type
-      .map(t => `<span class="pokemon-type background-${t}">${t}</span>`)
-      .join('')
-    const img = pokemon.name.replace(/['\.]/g, '').replace(/\s/g, '-')
-    return `<div class="pokemon" data-name="${pokemon.name}" data-type="${pokemon.type}" tabindex="${pokemon.id}">
+      .map((t) => `<span class="pokemon-type background-${t}">${t}</span>`)
+      .join("");
+    const img = pokemon.name.replace(/['\.]/g, "").replace(/\s/g, "-");
+    return `<div class="pokemon" data-name="${
+      pokemon.name
+    }" data-type="${pokemon.type}" tabindex="${pokemon.id}">
     <div class="btn-toolbar float-right mt-2" role="toolbar" aria-label="Toolbar with button groups">
       <div class="btn-group btn-group-sm mr-2" role="group" aria-label="First group">
-        <button type="button" class="btn btn-link btn-edit" data-id="${pokemon.id}">
+        <button type="button" class="btn btn-link btn-edit" data-id="${
+          pokemon.id
+        }">
           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
           </svg>
         </button>
-        <button type="button" class="btn btn-link btn-trash" data-id="${pokemon.id}">
+        <button type="button" class="btn btn-link btn-trash" data-id="${
+          pokemon.id
+        }">
           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -155,33 +193,42 @@ $(document).ready(function () {
         <img src="img/${img.toLowerCase()}.png" alt="${pokemon.name}">
       </figure>
       <section class="pokemon-description">
-        <span class="pokemon-id">#${Number(pokemon.id).toString().padStart(3, '0')}</span>
+        <span class="pokemon-id">#${Number(pokemon.id)
+          .toString()
+          .padStart(3, "0")}</span>
         <h1 class="pokemon-name">${pokemon.name}</h1>
         <div class="pokemon-types">${types}</div>
       </section>
       <section class="pokemon-stats">${loadStats(pokemon.stats)}</section>
-    </div>`
+    </div>`;
   }
 
   function loadTypes(types) {
-    types.map(t => $('#filter-type').append(`<option>${t}</option>`))
+    types.map((t) => $("#filter-type").append(`<option>${t}</option>`));
 
-    types.map(t => $('.ckb-types').append(`<div class="form-check form-check-inline"><input class="form-check-input type" type="checkbox"" value="${t}"><label class="form-check-label">${t}</label></div>`))
+    types.map((t) =>
+      $(".ckb-types").append(
+        `<div class="form-check form-check-inline"><input class="form-check-input type" type="checkbox"" value="${t}"><label class="form-check-label">${t}</label></div>`
+      )
+    );
   }
 
   function loadStats(stats) {
     return Object.entries(stats)
-      .filter(([name, value]) => !['total'].includes(name))
-      .map(([name, value]) =>
-        `<div class="stat-row">
+      .filter(([name, value]) => !["total"].includes(name))
+      .map(
+        ([name, value]) =>
+          `<div class="stat-row">
         <div>${name}</div>
         <div class="stat-bar">
-          <div class="stat-bar-bg" style="width: ${100 * value / 250}%">${value}</div>
+          <div class="stat-bar-bg" style="width: ${
+            (100 * value) / 250
+          }%">${value}</div>
         </div>
       </div>`
       )
-      .join('')
+      .join("");
   }
 
-  loadTypes(pokemonTypes)
+  loadTypes(pokemonTypes);
 });
