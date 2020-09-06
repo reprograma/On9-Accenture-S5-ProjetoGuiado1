e1825878dd4f6d9ba73d1e53ba4dbfbfc3b16fbe
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-09-06 17:03:18 -0300

    adicionar função de filtro e ordenação ao projeto

--- pokedex/js/app.js
+++ pokedex/js/app.js
@@ -1,36 +1,140 @@
 let pokemonList = [];
 
 function filterPokemon(name, type) {
-    // Seu código aqui
+  const listaFiltrada = pokemonList.filter((pokemon) => {
+    const searchName = new RegExp(name, "i");
+    const checkName = searchName.test(pokemon.name);
+    const checkType = type.length == 0 ? true : pokemon.type.includes(type);
+    return checkName && checkType;
+  });
 
-    // Retorne a lista filtrada
-    return [];
+  return listaFiltrada;
 }
 
+/* Primeira forma de fazer a função de ordenação
 function sortPokemon(filteredList, sortExpression) {
-    // Seu código aqui
+  let listaOrdenada = [];
 
-    // Retorne a lista ordenada
-    return [];
+  if (sortExpression == "ID (asc)") {
+    listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+      return pokemonA.id - pokemonB.id;
+    });
+  }
+
+  if (sortExpression == "ID (desc)") {
+    listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+      return pokemonA.id - pokemonB.id;
+    });
+    listaOrdenada.reverse();
+  }
+
+  if (sortExpression == "A-Z") {
+    listaOrdenada = filteredList.sort(function (pokemonA, pokemonB) {
+      if (pokemonA.name > pokemonB.name) {
+        return 1;
+      }
+      if (pokemonA.name < pokemonB.name) {
+        return -1;
+      }
+      // a deve ser igual a b
+      return 0;
+    });
+  }
+
+  if (sortExpression == "Z-A") {
+    listaOrdenada = filteredList.sort(function (pokemonA, pokemonB) {
+      if (pokemonA.name > pokemonB.name) {
+        return -1;
+      }
+      if (pokemonA.name < pokemonB.name) {
+        return 1;
+      }
+      // a deve ser igual a b
+      return 0;
+    });
+  }
+  return listaOrdenada;
+} */
+
+// Segunda forma de fazer a função de ordenação
+function sortPokemon(filteredList, sortExpression) {
+  let listaOrdenada = [];
+  console.log(sortExpression);
+  switch (sortExpression) {
+    case "ID (desc)":
+      listaOrdenada = filteredList.sort(function (pokemonA, pokemonB) {
+        return pokemonA.id - pokemonB.id;
+      });
+      listaOrdenada.reverse();
+      break;
+    case "A-Z":
+      listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+        if (pokemonA.name > pokemonB.name) {
+          return 1;
+        }
+        if (pokemonA.name < pokemonB.name) {
+          return -1;
+        }
+        return 0;
+      });
+      break;
+    case "Z-A":
+      listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+        if (pokemonA.name > pokemonB.name) {
+          return -1;
+        }
+        if (pokemonA.name < pokemonB.name) {
+          return 1;
+        }
+        return 0;
+      });
+      break;
+    default:
+      listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+        return pokemonA.id - pokemonB.id;
+      });
+      break;
+  }
+
+  return listaOrdenada;
 }
 
 function deletePokemon(id) {
-    // Seu código aqui
+  // Seu código aqui
 }
 
-function addPokemon(name, hp, attack, defense, speed, specialAttack, specialDefense, types) {
-    // Seu código aqui
-    // Atenção: types vem como uma stringona, cabe a você transformar num array
+function addPokemon(
+  name,
+  hp,
+  attack,
+  defense,
+  speed,
+  specialAttack,
+  specialDefense,
+  types
+) {
+  // Seu código aqui
+  // Atenção: types vem como uma stringona, cabe a você transformar num array
 }
 
 function getPokemon(id) {
-    // Seu código aqui
+  // Seu código aqui
 
-    // Retorne um pokemon da lista que tenha o id enviado
-    return {};
+  // Retorne um pokemon da lista que tenha o id enviado
+  return {};
 }
 
-function editPokemon(id, name, hp, attack, defense, speed, specialAttack, specialDefense, types) {
-    // Seu código aqui
-    // Atenção: types vem como uma stringona, cabe a você transformar num array
+function editPokemon(
+  id,
+  name,
+  hp,
+  attack,
+  defense,
+  speed,
+  specialAttack,
+  specialDefense,
+  types
+) {
+  // Seu código aqui
+  // Atenção: types vem como uma stringona, cabe a você transformar num array
 }
