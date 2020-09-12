35b6d36703f1205bc544ace00aa2c474e42517ee
gloriateodoro <gloriateodoro1@gmail.com>
2020-09-12 16:10:28 -0300

    Adicionando funções filterPokemon, sortPokemon, getPokemon, deletePokemon, addPokemon e editPokemon para o projeto da pokedex

--- pokedex/js/app.js
+++ pokedex/js/app.js
@@ -1,36 +1,136 @@
 let pokemonList = [];
 
-function filterPokemon(name, type) {
-    // Seu código aqui
-
+function filterPokemon(name, type) { //Prof fez
+const listafiltrada = pokemonList.filter(pokemon => {
+const searchName = new RegExp(name, 'i');
+const checkName = searchName.test(pokemon.name);
+        // condicão ? atente se verdadeiro : atende se falso
+const listaTipos = pokemon.type
+const checkType = type.length == 0 ? true : pokemon.type.includes(type);
+        return checkName && checkType;
+    })
     // Retorne a lista filtrada
-    return [];
+    return listafiltrada;
 }
 
-function sortPokemon(filteredList, sortExpression) {
-    // Seu código aqui
 
-    // Retorne a lista ordenada
-    return [];
+
+
+
+function sortPokemon(filteredList, sortExpression) { //Prof fez
+    let listaOrdenada;
+
+    switch (sortExpression) {
+        case 'ID (asc)':
+            listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+                return pokemonA.id - pokemonB.id;
+            });
+            break;
+
+        case 'ID (desc)':
+            listaOrdenada = filteredList.sort(function (pokemonA, pokemonB) {
+                return pokemonA.id - pokemonB.id;
+            });
+            listaOrdenada.reverse();
+            break;
+
+        case 'A-Z':
+            listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+                if (pokemonA.name > pokemonB.name) {
+                    return 1;
+                }
+                if (pokemonA.name < pokemonB.name) {
+                    return -1;
+                }
+                return 0;
+            });
+            break;
+
+        case 'Z-A':
+            listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+                if (pokemonA.name > pokemonB.name) {
+                    return -1;
+                }
+                if (pokemonA.name < pokemonB.name) {
+                    return 1;
+                }
+                return 0;
+            });
+            break;
+        default:
+            listaOrdenada = filteredList.sort((pokemonA, pokemonB) => {
+                return pokemonA.id - pokemonB.id;
+            });
+            break;
+
+    }
+        // Retorne a lista ordenada
+        return listaOrdenada;
 }
+
+
+
+
+function getPokemon(id) {  //prof fez
+    const pokemon = pokemonList.find(pokemon => pokemon.id == id);
+    return pokemon;
+}
+
+
+
+
 
 function deletePokemon(id) {
-    // Seu código aqui
+    
+    let pokemon = getPokemon(id);
+    console.log(pokemon);
+    let indice = pokemonList.indexOf(pokemon);
+    console.log(indice);
+    return pokemonList.splice(indice, 1);
+
+
 }
 
 function addPokemon(name, hp, attack, defense, speed, specialAttack, specialDefense, types) {
-    // Seu código aqui
-    // Atenção: types vem como uma stringona, cabe a você transformar num array
+    
+    types = types.split(";");
+    types = types.slice(0, types.length - 1);
+    const pokemon = {
+        name: name,
+        stats:{
+            hp: hp,
+            attack: attack,
+            defense: defense,
+            speed: speed,
+            'sp-atk': specialAttack,
+            'sp-def': specialDefense,
+        },
+        type: types,
+        id: (pokemonList.length + 1)
+
+}
+    return pokemonList.push(pokemon)
 }
 
-function getPokemon(id) {
-    // Seu código aqui
 
-    // Retorne um pokemon da lista que tenha o id enviado
-    return {};
-}
+function editPokemon(id, name, hp, attack, defense, speed, specialAttack, specialDefense, types){
 
-function editPokemon(id, name, hp, attack, defense, speed, specialAttack, specialDefense, types) {
-    // Seu código aqui
-    // Atenção: types vem como uma stringona, cabe a você transformar num array
-}
+let pokemonIndex = pokemonList.findIndex(pokemon => pokemon.id == id);
+
+types = types.split(";"); //porque types vem como uma stringona e preciso de um array
+types = types.slice(0, types.length - 1);
+
+pokemonList[pokemonIndex] = { //name, type e id ficam separados pois são únicos
+    name: name,
+    stats: {
+        hp: hp,
+        attack: attack,
+        defense: defense,
+        speed: speed,
+        'sp-atk': specialAttack,
+        'sp-def': specialDefense, 
+    },
+    type: types,
+    id: id
+
+}}
