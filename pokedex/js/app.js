6bc771af96237f00ac53b6d483beaddd043d01aa
EllenCoe <ellen.xavier1@hotmail.com>
2020-09-12 19:58:56 -0300

    Adicionando funçoes de delete, edit e add

--- pokedex/js/app.js
+++ pokedex/js/app.js
@@ -1,5 +1,14 @@
 let pokemonList = [];
-
+/*function filterPokemon(name, type) {
+  const listafiltrada = pokemonList.filter((pokemon) => {
+    const searchName = new RegExp(name, "i");
+    const checkName = searchName.test(pokemon.name);
+    // condicão ? atente se verdadeiro : atende se falso
+    const checkType = type.length == 0 ? true : pokemon.type.includes(type);
+    return checkName && checkType;
+  });
+  return listafiltrada;
+}*/
 function filterPokemon(name, type, pokemonList) {
   let list = pokemonList;
 
@@ -41,25 +50,13 @@ function sortPokemon(filteredList, sortExpression) {
   switch (sortExpression) {
     case "ID (asc)":
       return list.sort(function (currentItem, anteriorItem) {
-        if (currentItem.id < anteriorItem.id) {
-          return -1;
-        }
-        if (currentItem.id > anteriorItem.id) {
-          return 1;
-        }
-        return 0;
+        return currentItem.id - anteriorItem.id;
       });
 
     case "ID (desc)":
       return list
         .sort(function (currentItem, anteriorItem) {
-          if (currentItem.id < anteriorItem.id) {
-            return -1;
-          }
-          if (currentItem.id > anteriorItem.id) {
-            return 1;
-          }
-          return 0;
+          return currentItem.id - anteriorItem.id;
         })
         .reverse();
     case "A-Z":
@@ -90,7 +87,13 @@ function sortPokemon(filteredList, sortExpression) {
 }
 
 function deletePokemon(id) {
-  // Seu código aqui
+  const index = pokemonList.findIndex((pokemon) => {
+    return pokemon.id == id;
+  });
+
+  if (index >= 0) {
+    pokemonList.splice(index, 1);
+  } // Seu código aqui
 }
 
 function addPokemon(
@@ -103,15 +106,38 @@ function addPokemon(
   specialDefense,
   types
 ) {
-  // Seu código aqui
-  // Atenção: types vem como uma stringona, cabe a você transformar num array
+  let newPokemon = Object();
+
+  const listById = pokemonList.map((pokemon) => pokemon.id);
+  const maxValue = Math.max.apply(null, listById);
+
+  const listTypes = types.split(";").filter((type) => type != "");
+
+  newPokemon = {
+    id: maxValue + 1,
+    name: name,
+    type: listTypes,
+    stats: {
+      hp: hp,
+      attack: attack,
+      defense: defense,
+      speed: speed,
+      "sp-atk": specialAttack,
+      "sp-def": specialDefense,
+    },
+  };
+
+  pokemonList.push(newPokemon);
+  alert("Pokemon adicionado com sucesso!");
 }
 
 function getPokemon(id) {
-  // Seu código aqui
-
-  // Retorne um pokemon da lista que tenha o id enviado
-  return {};
+  const pokemon = pokemonList.find((pokemon) => pokemon.id == id);
+  if (pokemon) {
+    return pokemon;
+  } else {
+    return {};
+  }
 }
 
 function editPokemon(
@@ -125,6 +151,19 @@ function editPokemon(
   specialDefense,
   types
 ) {
-  // Seu código aqui
-  // Atenção: types vem como uma stringona, cabe a você transformar num array
+  let pokemon = getPokemon(id);
+  let listTypes = types.split(";");
+  let index = listTypes.indexOf("");
+  listTypes.splice(index, 1);
+
+  pokemon.name = name;
+  pokemon.stats.hp = hp;
+  pokemon.stats.attack = attack;
+  pokemon.stats.defense = defense;
+  pokemon.stats.speed = speed;
+  pokemon["stats"]["sp-atk"] = specialAttack;
+  pokemon["stats"]["sp-def"] = specialDefense;
+  pokemon.type = listTypes;
+
+  alert("Pokemon editado com sucesso!");
 }
